c7735b7e5f0fb9a2676c30a51f01ecea5b87c38a
polock1337 <31995779+polock1337@users.noreply.github.com>
2018-04-26 10:49:19 -0400

    ajout diapo

--- site/script/boutonBalancier.js
+++ site/script/boutonBalancier.js
@@ -2,18 +2,42 @@
 
 
 
+var finVignietteLancia;
+var boutonBalancierLancia;
+
+var finVignietteSubaru;
+var boutonBalancierSubaru;
+
 var finVigniette;
-var boutonBalancier;
+var boutonBalancierMitsubishi;
 
-
-function initialiserBouton()
+function initialiser()
 {
+    //lancia
+    finVignietteLancia = document.getElementById("fin-vigniette-Lancia");
+    boutonBalancierLancia = document.getElementById("bouton-balancier-Lancia");
     
-    finVigniette = document.getElementById("fin-vigniette");
-    boutonBalancier = document.getElementById("bouton-balancier-Lancia");
+    boutonBalancierLancia.onclick = balancerTexteFinVignetteLancia;
+    boutonBalancierLancia.ouvert = false;
     
-    boutonBalancier.onclick = balancerTexteFinVignette;
-    boutonBalancier.ouvert = false;
+    
+    //Subaru
+    finVignietteSubaru = document.getElementById("fin-vigniette-Subaru");
+    boutonBalancierSubaru = document.getElementById("bouton-balancier-Subaru");
+    
+    boutonBalancierSubaru.onclick = balancerTexteFinVignetteSubaru;
+    boutonBalancierSubaru.ouvert = false;
+    
+    
+    //mishibishi
+    finVignietteMitsubishi = document.getElementById("fin-vigniette-Mitsubishi");
+    boutonBalancierMitsubishi = document.getElementById("bouton-balancier-Mitsubishi");
+    
+    boutonBalancierMitsubishi.onclick = balancerTexteFinVignetteMitsubishi;
+    boutonBalancierMitsubishi.ouvert = false;
+    
+    
+    
     
     
 }
@@ -23,20 +47,56 @@ function initialiserBouton()
 
 
 
-function balancerTexteFinVignette(evenement)
+function balancerTexteFinVignetteLancia(evenement)
 {
-    if(boutonBalancier.ouvert)
+    if(boutonBalancierLancia.ouvert)
     {
         evenement.preventDefault();
-        finVigniette.style.display="none";
-        bouton-balancier-Lancia.innerHTML = "+";
-        boutonBalancier.ouvert = false;
+        finVignietteLancia.style.display="none";
+        boutonBalancierLancia.innerHTML = "+";
+        boutonBalancierLancia.ouvert = false;
     }
     else
     {
         evenement.preventDefault();
-        finVigniette.style.display="inline";
-        boutonBalancier.innerHTML = "-";
-        boutonBalancier.ouvert = true;
+        finVignietteLancia.style.display="inline";
+        boutonBalancierLancia.innerHTML = "-";
+        boutonBalancierLancia.ouvert = true;
+    }
+}
+
+function balancerTexteFinVignetteSubaru(evenement)
+{
+    if(boutonBalancierSubaru.ouvert)
+    {
+        evenement.preventDefault();
+        finVignietteSubaru.style.display="none";
+        boutonBalancierSubaru.innerHTML = "+";
+        boutonBalancierSubaru.ouvert = false;
+    }
+    else
+    {
+        evenement.preventDefault();
+        finVignietteSubaru.style.display="inline";
+        boutonBalancierSubaru.innerHTML = "-";
+        boutonBalancierSubaru.ouvert = true;
+    }
+}
+
+function balancerTexteFinVignetteMitsubishi(evenement)
+{
+    if(boutonBalancierMitsubishi.ouvert)
+    {
+        evenement.preventDefault();
+        finVignietteMitsubishi.style.display="none";
+        boutonBalancierMitsubishi.innerHTML = "+";
+        boutonBalancierMitsubishi.ouvert = false;
+    }
+    else
+    {
+        evenement.preventDefault();
+        finVignietteMitsubishi.style.display="inline";
+        boutonBalancierMitsubishi.innerHTML = "-";
+        boutonBalancierMitsubishi.ouvert = true;
     }
 }
